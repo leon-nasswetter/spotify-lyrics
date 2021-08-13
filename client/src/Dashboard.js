a06d78f48d68b3481686b0df306e001ee66f39f7
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-08-13 16:32:18 -0700

    wip search and display functional

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,13 +1,59 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useAuth from "./useAuth";
+import TrackSearchResult from "./TrackSearchResult"
 import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "4ff893483d42477baf64cb409296c766",
+});
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+//   console.log(searchResults)
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    //  cancel to only make request once stop typing
+    let cancel = false;
+    spotifyApi.searchTracks(search).then((res) => {
+      if (cancel) return;
+      //   console.log(res)
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          //    finding smallest image
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
+
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
+
+    return () => (cancel = true);
+  }, [search, accessToken]);
 
   return (
-    <Container className="d-flex flex-column  py-2" style={{ height: "100vh"}}>
+    <Container className="d-flex flex-column  py-2" style={{ height: "100vh" }}>
       <Form.Control
         type="search"
         placeholder="Search"
@@ -15,10 +61,12 @@ export default function Dashboard({ code }) {
         onChange={(e) => setSearch(e.target.value)}
       />
 
-        <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
-            Songs
-        </div>
-        <div>Bottom</div>
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+        {searchResults.map(track => (
+            <TrackSearchResult track={track} key={track.uri} />
+        ))}
+      </div>
+      <div>Bottom</div>
     </Container>
   );
 }
