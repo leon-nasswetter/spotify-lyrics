f3d8a69cd0bb88ec71f0640b548ba01b9d37a250
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-08-13 15:22:28 -0700

    big wip commit after finally adding env variables

---
+++ client/src/Dashboard.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+
+export default function Dashboard({ code }) {
+  const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+
+  return (
+    <Container className="d-flex flex-column  py-2" style={{ height: "100vh"}}>
+      <Form.Control
+        type="search"
+        placeholder="Search"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+        <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
+            Songs
+        </div>
+        <div>Bottom</div>
+    </Container>
+  );
+}
